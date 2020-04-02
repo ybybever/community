2bda6ab6f82268588254365ce4352124288ecb3b
ybybever <1461971911@qq.com>
2020-04-02 12:25:51 +0800

    init repo

---
+++ src/test/java/com/ybyb/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ybyb.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
